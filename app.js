c89ad8ac4098864e6aa5f24c150ea754bcc01e76
vikalp varshney <99534514+vikalpvarshney1@users.noreply.github.com>
2022-04-15 14:53:38 +0530

    .

---
+++ app.js
@@ -0,0 +1,18 @@
+function sum(a,b){
+return a+b;
+}
+function sub(a, b) {
+    return a - b;
+}
+function mul(a, b) {
+    return a * b;
+}
+function div(a, b) {
+    return a/b;
+}
+module.exports={
+    sum,
+    sub,
+    mul,
+    div
+}
---
+++ index.js
@@ -0,0 +1,5 @@
+const app=require('./app')
+console.log(app.sum(2,3))
+console.log(app.sub(2, 3))
+console.log(app.mul(2, 3))
+console.log(app.div(2, 3))
